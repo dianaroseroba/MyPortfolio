560edf40a5faa35e8966514d9f6558efa57cf47e
Diana Rosero Bastidas <diana.roseroba@campusucc.edu.co>
2024-05-17 20:57:30 -0500

    funcionalidad de boton enviar mensajes

--- src/screens/ContactMe.jsx
+++ src/screens/ContactMe.jsx
@@ -1,8 +1,35 @@
 import { FaPhone, FaLocationDot, FaEnvelope } from "react-icons/fa6";
 import { useTranslation } from 'react-i18next';
+import { useState } from 'react';
+import emailjs from 'emailjs-com';
 
 const ContactMe = () => {
     const { t } = useTranslation();
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        emailjs.send('service_d8yunjk', 'template_sfghhl3', formData, 'aT3K1-OPw8LDetakL')
+            .then((response) => {
+                console.log('SUCCESS!', response.status, response.text);
+                alert('Mensaje enviado con éxito');
+            }, (error) => {
+                console.error('FAILED...', error);
+                alert('Error al enviar el mensaje');
+            });
+    }
 
     return (
         <section className="grid md:grid-cols-2">
@@ -26,19 +53,45 @@ const ContactMe = () => {
             </section>
 
             <section className="md:w-full md:max-w-md md:mt-4">
-                <form className="bg-personalBlue-50 rounded-3xl px-8 mb-2">
+                <form className="bg-personalBlue-50 rounded-3xl px-8 mb-2" onSubmit={handleSubmit}>
                     <h1 className="text-colorLetter text-center text-3xl font-bold">{t('contacMessageForm')}</h1>
                     <section className="mb-2 mt-2">
-                        <input className="rounded-2xl w-full py-2 px-3 bg-personalBlue-50 focus:outline-none focus:shadow-outline border border-colorLetter" id="username" type="text" placeholder={t('contacFormName')} />
+                        <input
+                            className="rounded-2xl w-full py-2 px-3 bg-personalBlue-50 focus:outline-none focus:shadow-outline border border-colorLetter"
+                            id="name"
+                            name="name"
+                            type="text"
+                            placeholder={t('contacFormName')}
+                            value={formData.name}
+                            onChange={handleChange}
+                        />
                     </section>
                     <section className="mb-2">
-                        <input className="rounded-2xl w-full py-2 px-3 bg-personalBlue-50 focus:outline-none focus:shadow-outline border border-colorLetter" id="username" type="text" placeholder={t('contacFormEmail')} />
+                        <input
+                            className="rounded-2xl w-full py-2 px-3 bg-personalBlue-50 focus:outline-none focus:shadow-outline border border-colorLetter"
+                            id="email"
+                            name="email"
+                            type="email"
+                            placeholder={t('contacFormEmail')}
+                            value={formData.email}
+                            onChange={handleChange}
+                        />
                     </section>
                     <section className="mb-4">
-                        <input className="h-44 rounded-2xl w-full py-2 px-3 bg-personalBlue-50 focus:outline-none border border-colorLetter" id="username" type="text" placeholder={t('contacFormMessage')} />
+                        <textarea
+                            className="h-44 rounded-2xl w-full py-2 px-3 bg-personalBlue-50 focus:outline-none border border-colorLetter"
+                            id="message"
+                            name="message"
+                            placeholder={t('contacFormMessage')}
+                            value={formData.message}
+                            onChange={handleChange}
+                        />
                     </section>
                     <section className="flex items-center justify-center">
-                        <button className="bg-colorLetter mb-4 hover:bg-colorLetter text-personalBlue-100 text-white font-bold py-1 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                        <button
+                            className="bg-colorLetter mb-4 hover:bg-colorLetter text-personalBlue-100 text-white font-bold py-1 px-4 rounded focus:outline-none focus:shadow-outline"
+                            type="submit"
+                        >
                             {t('contacButton')}
                         </button>
                     </section>
