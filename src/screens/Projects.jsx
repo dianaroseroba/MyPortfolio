4ac8dcc55886eddbb7a25a8c8d658dc7ffd5c1ed
Diana Rosero Bastidas <diana.roseroba@campusucc.edu.co>
2024-05-29 09:04:16 -0500

    editando link de portafolio

--- src/screens/Projects.jsx
+++ src/screens/Projects.jsx
@@ -12,7 +12,7 @@ const Projects = () => {
         {
             title: t('firstTitleProj'),
             description: t('firstDescProj'),
-            githubLink: 'https://github.com/dianaroseroba/PortfolioDianaRosero',
+            githubLink: 'https://github.com/dianaroseroba/MyPortfolio.git',
             demoLink: 'https://tu-demo.com',
             imageSrc: portfolio
         },
